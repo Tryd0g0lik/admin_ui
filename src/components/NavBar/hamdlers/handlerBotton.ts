9e7b82093ead9418ff9c44fb99c1758b0477eed8
Tryd0g0lik <work80@mail.ru>
2025-04-30 08:06:11 +0700

    [master]: the handlerButtonLoginOut handler of event was added

--- src/components/NavBar/hamdlers/handlerBotton.ts
+++ src/components/NavBar/hamdlers/handlerBotton.ts
@@ -1,5 +1,28 @@
 import React from "react";
 
-export function handlerButtonLogin(e: React.MouseEvent): boolean {
+/**
+ * This handler is  listenning the click event by button \
+ * from the <header> tag.\
+ * Here is only chacnge thr text of button by click event.\
+ * @param e : React.MouseClick- event object
+ * @returns boolean
+ */
+export function handlerButtonLoginOut(e: React.MouseEvent): boolean {
+  /* CHECK THE PALCE OF TARGET*/
+  if ((!e.type) || (
+    (e.type && e.type.toLowerCase() !== 'click') ||
+    (e.type.toLowerCase() === 'click' && !(
+      (e.target as HTMLElement).className.toLowerCase().includes("button__click"))
+    ))) {
+    return false;
+  }
+  e.stopPropagation();
+  const target = (e.target as HTMLAnchorElement);
+  /** THE AUTO CHANGE BUTTON IS BELOW BY CLICK EVENT */
+  if (target.outerText.toLowerCase() === 'вход') {
+    target.innerText = "Выход";
+  } else if (target.outerText.toLowerCase() === 'выход') {
+    target.innerHTML = "Вход";
+  }
 return true;
 }
