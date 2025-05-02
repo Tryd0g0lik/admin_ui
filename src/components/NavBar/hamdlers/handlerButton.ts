a9bcf25145b61d5a3f41288f7c7006c2b9b2ae05
Tryd0g0lik <work80@mail.ru>
2025-05-02 13:52:51 +0700

    [master]: the file name was before the 'handlerBotton' and after renaming is handlerButton

---
+++ src/components/NavBar/hamdlers/handlerButton.ts
@@ -0,0 +1,24 @@
+import React from "react";
+
+/**
+ * This handler is  listenning the click event by button \
+ * from the <header> tag.\
+ * Here is only chacnge thr text of button by click event.\
+ * @param e : React.MouseClick- event object
+ * @returns boolean
+ */
+export function handlerButtonLoginOut(e: React.MouseEvent): boolean {
+  /* CHECK THE PALCE OF TARGET*/
+  if ((!e.type) || (
+    (e.type && e.type.toLowerCase() !== 'click') ||
+    (e.type.toLowerCase() === 'click' && !(
+      (e.target as HTMLElement).className.toLowerCase().includes("button__click"))
+    ))) {
+    return false;
+  }
+  e.stopPropagation();
+  if ((e.target as HTMLElement).outerText.toLowerCase() === 'выход') {
+    return false;
+  }
+return true;
+}
